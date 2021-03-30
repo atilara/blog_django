f4db8e53a10ef994fba17b97414122915f5a965a
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-30 16:46:24 -0300

    feat: create a page to detail posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    # Passando pk como paramêtro
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
